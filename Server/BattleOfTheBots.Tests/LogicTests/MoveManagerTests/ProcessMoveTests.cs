2929fd3527ad710271ef571f49830a01f18d9dd6
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-10-12 16:17:40 +0100

    Test infrastructure

---
+++ Server/BattleOfTheBots.Tests/LogicTests/MoveManagerTests/ProcessMoveTests.cs
@@ -0,0 +1,22 @@
+﻿using BattleOfTheBots.Logic;
+using NUnit.Framework;
+using System;
+
+namespace BattleOfTheBots.Tests.LogicTests.MoveManagerTests
+{
+    public class ProcessMoveTests
+    {
+        public ProcessMoveTests()
+        {
+            this.MoveManager = new MoveManager();
+        }
+
+        [Test]
+        public void CheckMove()
+        {
+
+        }
+
+        public MoveManager MoveManager { get; }
+    }
+}
