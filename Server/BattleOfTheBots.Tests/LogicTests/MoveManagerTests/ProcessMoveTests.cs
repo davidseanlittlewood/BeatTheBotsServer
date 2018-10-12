728c39c2a3baaef888b3aa36675da6cd056cfbfe
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-10-12 16:34:29 +0100

    Test refactor

--- Server/BattleOfTheBots.Tests/LogicTests/MoveManagerTests/ProcessMoveTests.cs
+++ Server/BattleOfTheBots.Tests/LogicTests/MoveManagerTests/ProcessMoveTests.cs
@@ -16,13 +16,23 @@ namespace BattleOfTheBots.Tests.LogicTests.MoveManagerTests
         [Test]
         public void CheckMove()
         {
-            var arena = new Arena(new Bot[] { new Bot { Name = Guid.NewGuid().ToString(), Position = 3 }, new Bot { Name = Guid.NewGuid().ToString(), Position = 4 } });
-            this.MoveManager.ProcessMove(arena, new BotMove(arena.Bots.First(), Move.MoveForwards), new BotMove(arena.Bots.Last(), Move.MoveForwards));
+            this.MoveManager.ProcessMove(this.Arena, new BotMove(this.FirstBot, Move.MoveForwards), new BotMove(this.LastBot, Move.MoveForwards));
 
-            Assert.AreEqual(3, arena.Bots.First().Position);
-            Assert.AreEqual(4, arena.Bots.Last().Position);
+            Assert.AreEqual(3, this.FirstBot.Position);
+            Assert.AreEqual(4, this.LastBot.Position);
         }
 
         public MoveManager MoveManager { get; }
+        public Arena Arena { get; set; }
+
+        public Bot FirstBot => this.Arena?.Bots?.FirstOrDefault();
+        public Bot LastBot => this.Arena?.Bots?.LastOrDefault();
+
+
+        [SetUp]
+        public void RestoreArena()
+        {
+            this.Arena = new Arena(new Bot[] { new Bot { Name = Guid.NewGuid().ToString(), Position = 3 }, new Bot { Name = Guid.NewGuid().ToString(), Position = 4 } });
+        }
     }
 }
