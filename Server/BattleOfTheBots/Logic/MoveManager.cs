f1f64693e2bbfc10e56a8043ddc522c5e9f3c2a0
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-10-18 17:22:16 +0100

    Improving movements

--- Server/BattleOfTheBots/Logic/MoveManager.cs
+++ Server/BattleOfTheBots/Logic/MoveManager.cs
@@ -58,16 +58,21 @@ namespace BattleOfTheBots.Logic
 
         private void ProcessAxeDamage(Arena arena, BotMove botA, BotMove botB)
         {
-            if(AreSideBySide(botA, botB)) // only deal damage if they're side by side
+            if (AreSideBySide(botA, botB)) // only deal damage if they're side by side
             {
-                TheBotTakesDamage(botA, arena.AxeDamage);
-                TheBotTakesDamage(botB, arena.AxeDamage);
+                if (botB.Move == Move.AttackWithAxe) TheBotTakesDamage(botA, arena.AxeDamage);
+                if (botA.Move == Move.AttackWithAxe) TheBotTakesDamage(botB, arena.AxeDamage);
             }
         }
 
         private bool AreSideBySide(BotMove botA, BotMove botB)
         {
             return botA.Bot.Position + 1 == botB.Bot.Position;
+        }
+
+        private bool AreSeperatedByOneSpace(BotMove botA, BotMove botB)
+        {
+            return botA.Bot.Position + 2 == botB.Bot.Position;
         }
 
         private void CheckForVictory(Arena arena, BotMove botA, BotMove botB)
@@ -81,40 +86,111 @@ namespace BattleOfTheBots.Logic
 
         private void MoveAndShunt(Arena arena, BotMove botA, BotMove botB)
         {
-            if(botA.Move == Move.MoveForwards)
+            // Check for bots getting in each others' way and things smacking into each other
+            if(ProcessCollisions(arena, botA, botB))
             {
-                // We can move bot A forward UNLESS bot B is there (or wants to be there)
-                if(botB.Bot.Position == botA.Bot.Position + 1 && botB.Move != Move.MoveBackwards) // Bot B is in front and not moving back
-                {
-                    // Bot A can't move because Bot B is in the way
-                    NothingHappens(botA);
-                    NothingHappens(botB);
-                }
-
-                if(botB.Bot.Position == botA.Bot.Position + 2 && botB.Move == Move.MoveForwards) // Bot B is a space away but both want to move into the same space
-                {
-                    NothingHappens(botA);
-                    NothingHappens(botB);
-                }
+                return;
             }
-            else if(botA.Move == Move.MoveBackwards) // you can always move backwards (although sometimes it may not be a great idea)
+
+
+            if (botA.Move == Move.MoveForwards)
+            {
+                TheBotMovesRight(botA);
+            }
+            else if(botA.Move == Move.MoveBackwards)
             {
                 TheBotMovesLeft(botA);
             }
 
-            if (botB.Move == Move.MoveForwards)
+            if(botB.Move == Move.MoveForwards)
             {
-                // We can move bot B forward UNLESS bot A is there (or wants to be there)
-                if (botA.Bot.Position == botB.Bot.Position - 1 && botA.Move != Move.MoveBackwards) // Bot A is in front and not moving back
-                {
-                    // Bot B can't move because Bot A is in the way
-                }
+                TheBotMovesLeft(botB);
             }
-            else if (botB.Move == Move.MoveBackwards) // you can always move backwards (although sometimes it may not be a great idea)
+            else if (botB.Move == Move.MoveBackwards)
             {
                 TheBotMovesRight(botB);
             }
         }
+
+        public bool ProcessCollisions(Arena arena, BotMove botA, BotMove botB)
+        {
+            // firstly you can only have a collison if you're both moving forward!
+            if ((botA.Move == Move.MoveForwards || botA.Move == Move.Shunt)
+                && (botB.Move == Move.MoveForwards || botB.Move == Move.Shunt))
+            {
+                if (AreSideBySide(botA, botB))
+                {
+                    if (botA.Move == Move.MoveForwards) // if BotA is moving forwards
+                    {
+                        if (botB.Move == Move.MoveForwards) // and BotB is moving forwards
+                        {
+                            // then don't do anything
+                        }
+                        else // however if Bot B is shunting
+                        {
+                            TheBotTakesDamage(botB, arena.ShuntDamage); // then BotB is damaged
+                            TheBotMovesLeft(botA); // and both bot A and bot B move left
+                            TheBotMovesLeft(botB);
+                        }
+                    }
+                    else // otherwise bot A is shunting
+                    {
+                        if (botB.Move == Move.MoveForwards) // and BotB is moving forwards
+                        {
+                            TheBotTakesDamage(botA, arena.ShuntDamage); // then Bot A is damaged
+                            TheBotMovesLeft(botA); // and both bot A and bot B move left
+                            TheBotMovesLeft(botB);
+                        }
+                        else // AND Bot B is shunting
+                        {
+                            TheBotTakesDamage(botA, arena.ShuntDamage); // both bots are damaged
+                            TheBotTakesDamage(botB, arena.ShuntDamage); // both bots are damaged
+                                                                        // but neither move
+                        }
+                    }
+                }
+                else if (AreSeperatedByOneSpace(botA, botB)) // if there's one space between us
+                {
+                    if (botA.Move == Move.MoveForwards) // if BotA is moving forwards into the space
+                    {
+                        if (botB.Move == Move.MoveForwards) // and BotB is moving forwards into the space
+                        {
+                            // then don't do anything (no one gets it)
+                        }
+                        else // however if Bot B is shunting
+                        {
+                            TheBotTakesDamage(botB, arena.ShuntDamage); // then BotB is damaged
+                            TheBotMovesLeft(botB); // and wins the space
+                        }
+                    }
+                    else // otherwise bot A is shunting
+                    {
+                        if (botB.Move == Move.MoveForwards) // and BotB is moving forwards
+                        {
+                            TheBotTakesDamage(botA, arena.ShuntDamage); // then Bot A is damaged
+                            TheBotMovesRight(botA); // bot A wins the space                            
+                        }
+                        else // AND Bot B is shunting
+                        {
+                            TheBotTakesDamage(botA, arena.ShuntDamage); // both bots are damaged
+                            TheBotTakesDamage(botB, arena.ShuntDamage); // both bots are damaged
+                                                                        // but neither move into the space
+                        }
+                    }
+                }
+                else // otherwise there's a big gap - no collision
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            else // we're not both moving forward - no collision
+            {
+                return false;
+            }            
+        }
+
 
 
         private void NothingHappens(BotMove bot)
