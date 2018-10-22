1ec45fa9473f8b698b4c8063e0f65a73760251a2
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-10-22 16:50:04 +0100

    Spltting out bot logic

---
+++ BotExample/BotExample/BotBaseClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+
+namespace BotExample
+{
+    public abstract class BotBaseClass
+    {
+        public string OpponentName { get; set; }
+        public string LastOpponentsMove { get; set; }
+        public bool Flipped { get; set; }
+        public int Health { get; set; }
+        public int Flips { get; set; }
+        public int Fuel { get; set; }
+        public int FlipOdds { get; set; }
+        public int ArenaSize { get; set; }
+        public char Direction { get; set; }
+
+        /* Method called when start instruction is received
+         *
+         * POST http://<your_bot_url>/start
+         *
+         */
+        public void SetStartValues(string opponentName, int health, int arenaSize, int flips, int flipOdds, int fuel, char direction)
+        {
+            OpponentName = opponentName;
+            Health = health;
+            ArenaSize = arenaSize;
+            Flips= flips;
+            FlipOdds = flipOdds;
+            Fuel = fuel;
+            Direction = direction;
+        }
+
+        /* Method called when move instruction is received instructing opponents move
+         *
+         * POST http://<your_bot_url>/move
+         *
+         */ 
+        public void SetLastOpponentsMove(string lastOpponentsMove)
+        {
+            LastOpponentsMove = lastOpponentsMove;
+        }
+
+        /* Method called when status instruction is received instructing 
+         *
+         * POST http://<your_bot_url>/status
+         *
+         */
+        public void SetFlippedStatus(bool flipped)
+        {
+            Flipped = flipped;
+        }
+
+        /* Method called when move instruction is received requesting your move
+         *
+         * GET http://<your_bot_url>/move
+         *
+         */
+        public virtual string GetMove()
+        {
+            return "HIT";            
+        }   
+    }
+}
---
+++ BotExample/BotExample/Bots/RandomBot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotExample.Bots
+{
+    public class RandomBot : BotBaseClass
+    {
+        public string OpponentName { get; set; }
+        public string LastOpponentsMove { get; set; }
+        public bool Flipped { get; set; }
+        public int Health { get; set; }
+        public int Flips { get; set; }
+        public int Fuel { get; set; }
+        public int FlipOdds { get; set; }
+        public int ArenaSize { get; set; }
+        public char Direction { get; set; }
+
+
+        public override string GetMove()
+        {
+            return GetRandomResponse();
+        }
+
+        public string GetRandomResponse()
+        {
+
+            Random random = new System.Random(Environment.TickCount);
+            int rnd = random.Next(6);
+            switch (rnd)
+            {
+                case 0:
+                    {
+                        return "FORWARD";
+                    }
+                case 1:
+                    {
+                        return "BACK";
+                    }
+                case 2:
+                    {
+                        return "HIT";
+                    }
+                case 3:
+                    {
+                        return "SHUNT";
+                    }
+                case 4:
+                    {
+                        Fuel--;
+                        return "FLAME";
+                    }
+                default:
+                    {
+                        Flips--;
+                        return "FLIP";
+                    }
+            }
+        }
+    }
+}
