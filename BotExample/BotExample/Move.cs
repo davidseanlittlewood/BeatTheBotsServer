25228f41dc06e2ba69a5ffce0a54612526035b97
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-10-22 17:10:48 +0100

    Move comments

--- BotExample/BotExample/Move.cs
+++ BotExample/BotExample/Move.cs
@@ -8,11 +8,34 @@ namespace BotExample
 {
     public enum Move
     {
+        /// <summary>
+        /// The Move provided if an error occoured
+        /// </summary>
         Invalid = 0,
+
+        /// <summary>
+        /// Attempt to flip your opponent
+        /// </summary>
         Flip,
+
+        /// <summary>
+        /// Charge forward at your opponent in an attempt to push them backwards
+        /// </summary>
         Shunt,
+
+        /// <summary>
+        /// Attack your opponent with an axe
+        /// </summary>
         AttackWithAxe,
+
+        /// <summary>
+        /// Move forwards steadily
+        /// </summary>
         MoveForwards,
+
+        /// <summary>
+        /// Move backwards cautiously
+        /// </summary>
         MoveBackwards
     }
 }
